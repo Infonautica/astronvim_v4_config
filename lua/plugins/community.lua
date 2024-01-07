0d341e3c74002167c7ea3dd87049e56fa9552dc9
Leonid Danilov <leonid989@gmail.com>
2024-01-07 11:30:59 +0200

    Init

---
+++ lua/plugins/community.lua
@@ -0,0 +1,7 @@
+return {
+  -- Add community imports to the `lua/config/lazy.lua` file in your Neovim configuration folder (typically ~/.config/nvim)
+  -- this guarantees that the community plugin specs are loaded before the user configuration
+
+  -- { import = "astrocommunity.color.transparent-nvim" },
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+}
---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,64 @@
+-- AstroCore provides a central place to modify mappings set up as well as which-key menu titles
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
+      n = {
+        -- second key is the lefthand side of the map
+
+        -- navigate buffer tabs with `H` and `L`
+        -- L = {
+        --   function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+        --   desc = "Next buffer",
+        -- },
+        -- H = {
+        --   function() require("atsrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+        --   desc = "Previous buffer",
+        -- },
+
+        -- mappings seen under group name "Buffer"
+        ["<leader>bD"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Pick to close",
+        },
+        -- tables with just a `desc` key will be registered with which-key if it's installed
+        -- this is useful for naming menus
+        ["<leader>b"] = { desc = "Buffers" },
+        -- quick save
+        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+        -- Disable shift based navigation to favor <C-u> and <C-d>
+        ["<S-Up>"] = { "<Nop>" },
+        ["<S-Down>"] = { "<Nop>" },
+
+        -- Jump to the end of line
+        ["<C-e>"] = { "$" },
+
+        -- Navigate splits
+        ["<Tab>"] = { ":wincmd w<CR>", desc = "Switch to next window" },
+        ["<C-Left>"] = { "<C-W>h", desc = "Jump to the left split" },
+        ["<C-Right>"] = { "<C-W>l", desc = "Jump to the right split" },
+        ["<C-Up>"] = { "<C-W>k", desc = "Jump to the up split" },
+        ["<C-Down>"] = { "<C-W>j", desc = "Jump to the down split" },
+
+        -- Create splits
+        ["<C-\\>"] = { ":vsplit<CR>", desc = "Split vertically" },
+        ["<C-->"] = { ":split<CR>", desc = "Split horizontally" },
+
+        -- Resize splits
+        ["<C-S-Left>"] = { ":vertical :resize +2<CR>", desc = "Increase width of the split" },
+        ["<C-S-Right>"] = { ":vertical :resize -2<CR>", desc = "Decrease width of the split" },
+        ["<C-S-Up>"] = { ":resize +2<CR>", desc = "Increase height of the split" },
+        ["<C-S-Down>"] = { ":resize -2<CR>", desc = "Decrease height of the split" },
+      },
+      v = {
+        ["<C-e>"] = { "$" }
+      },
+    },
+  },
+}
