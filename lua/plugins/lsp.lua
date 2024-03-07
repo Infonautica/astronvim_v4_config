1a78958e951347e2402abe98743b517ae58bacdd
Leonid Danilov <leonid989@gmail.com>
2024-03-07 15:28:26 +0200

    Added eslint, updated packages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,7 +32,7 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+      "eslint"
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
